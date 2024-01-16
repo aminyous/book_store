c3e2966b777b9c8a5191a047e737dba6d972c0d8
aminyous <amyn.hichri@gmail.com>
2024-01-16 08:44:42 +0100

    Using Json file

--- utils/database.py
+++ utils/database.py
@@ -1,35 +1,30 @@
+import json
+
 """ This is an interface between our app and data storage mechanism """
 
-books_file = "books.txt"
+books_file = "books.json"
 
 
 def create_book_table():
     with open(books_file, "w") as file:
-        pass
+        json.dump([], file)
 
 
 def add_book(name, author):
-    # with open(books_file, "r") as file:
-    #     books = file.readlines()
-    #
-    # books.append(f"{name},{author},0\n")
-    #
-    # with open(books_file, "w") as file:
-    #     file.writelines(books)
 
-    with open(books_file, "a") as file:
-        file.write(f"{name},{author},0\n")
+    books = get_all_books()
+    books.append({"name": name, "author": author, "read": False})
 
 
 def get_all_books():
 
     with open(books_file, "r") as file:
-        lines = [line.strip().split(',') for line in file.readlines()]
+        return json.load(file)
 
-    return [
-        {"name": line[0], "author": line[1], "read": line[2]}
-        for line in lines
-    ]
+
+def _save_all_books(books):
+    with open(books_file, "w") as file:
+        json.dump(books, file)
 
 
 def read_book(name):
@@ -37,14 +32,8 @@ def read_book(name):
 
     for book in books:
         if book["name"] == name:
-            book["read"] = "1"
+            book["read"] = True
     _save_all_books(books)
-
-
-def _save_all_books(books):
-    with open(books_file, "w") as file:
-        for book in books:
-            file.write(f"{book['name']},{book['author']},{book['read']}\n")
 
 
 def delete_book(name):
